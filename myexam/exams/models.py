59e0358f9f953978bfde20a571a9af76c17ab4a4
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-06-23 13:02:35 +0300

    feat: create model and make migration

--- myexam/exams/models.py
+++ myexam/exams/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Exam(models.Model):
+    title = models.CharField(max_length=255)  
+    created_at = models.DateTimeField(auto_now_add=True) 
+    exam_date = models.DateField()  
+    image = models.ImageField(upload_to='exam_images/') 
+    users = models.ManyToManyField(User, related_name='exams')  
+    is_public = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return self.title

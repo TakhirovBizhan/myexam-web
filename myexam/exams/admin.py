e5423398ab0cc01f8712653c48cf9d6d07a09977
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-06-23 13:07:20 +0300

    feat: admin panel is ready

--- myexam/exams/admin.py
+++ myexam/exams/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Exam
 
-# Register your models here.
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'exam_date', 'is_public', 'created_at')
+    search_fields = ('title', 'users__email')  
+    date_hierarchy = 'exam_date'  
+    list_filter = ('is_public', 'created_at')  
+    filter_horizontal = ('users',)
